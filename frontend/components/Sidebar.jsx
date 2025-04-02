1f189febf55f1a388f2490147a6570653d469e50
itisar-345 <143781880+itisar-345@users.noreply.github.com>
2025-04-02 23:05:52 +0530

    css update

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { Link } from "react-router-dom";
-import { X } from "lucide-react"; 
+import { FileType2, LibraryBig, FileCheck2, Library} from "lucide-react"; 
 import "./Sidebar.css"; 
 
 const Sidebar = ({ isOpen, onClose }) => {
@@ -10,13 +10,10 @@ const Sidebar = ({ isOpen, onClose }) => {
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}onClick={onClose}>
       <div className="sidebar-content" onClick={handleSidebarClick}>
-        <button onClick={onClose} className="close-btn">
-          <X size={24} /> {/* Lucide React close icon */}
-        </button>
         <ul className="menu">
-          <li><Link to="/resource-library" onClick={onClose}>Resource Library</Link></li>
-          <li><Link to="/resume-builder" onClick={onClose}>Resume Builder</Link></li>
-          <li><Link to="/ats-score-checker" onClick={onClose}>ATS Score Checker</Link></li>
+          <li><LibraryBig size={20} /><Link to="/resource-library" onClick={onClose}>Resource Library</Link></li>
+          <li><FileType2 size={20} /><Link to="/resume-builder" onClick={onClose}>Resume Builder</Link></li>
+          <li><FileCheck2 size={20} /><Link to="/ats-score-checker" onClick={onClose}>ATS Score Checker</Link></li>
         </ul>
       </div>
     </div>
